570f0b55d3abd9da5a01ed035ff8ae7b3ae39a86
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-07-12 20:39:16 +0100

    Made generate block function and resolved issues with .move() and .clear() above the board, but will need refactoring.

--- js/app.js
+++ js/app.js
@@ -13,9 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   buildBoard(board)
   const boardSquares = document.querySelectorAll('.board-square')
 
-
-  /// !!! ----- Working Here -----
-
+  // !!! Consider adding boardSquares to the parameters for the block class so that it can increase in purity and move up out of the dom to it's rightful place near the top of the code.
   class Block {
     constructor(homeIndex, possibleRotations) {
       this.homeIndex = homeIndex
@@ -31,7 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
       // !!! Change index name
       this.indexsOccupied.forEach(index => {
         // !!! Consider making next line pure re boardSquares
-        boardSquares[index].classList.remove('has-active-block', this.color)
+        // !!! The next line is VERY similar to something in move. Bring it out of both?
+        if (index >= 0) {
+          boardSquares[index].classList.remove('has-active-block', this.color)
+        }
       })
     }
     // !!! Change updateHome name to something to do with moving
@@ -49,6 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         case 'down':
           this.homeIndex += width
           break
+        default:
+          this.homeIndex
       }
     }
     /// !!! How similar is this to checkIfInWall and vice versa ???
@@ -63,17 +66,21 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       return true
     }
-    move(direction) {
-      if (this.checkCanMove(direction)) {
+    // !!! This isShifting stuff is a poor patchup join. Needs a fix
+    move(direction, isShifting) {
+      if (this.checkCanMove(direction ) || isShifting) {
         this.clearLastMove()
         this.updateHome(direction)
         /// !!! Consider changeing position name
         this.indexsOccupied.forEach(index => {
-          boardSquares[index].classList.add('has-active-block', this.color)
+          if (index >= 0) {
+            boardSquares[index].classList.add('has-active-block', this.color)
+          }
         })
       }
     }
     // !!! Update and move are doing awfully similar things. Intergrate somehow?
+    // !!! everything relating to the checks for movement and walls are super dodgey. Firstly they need to happen repeatedly. Secondly they won't work for anything other than walls (so not the blocks at the bottom), thirdly I've added this really dodgey isShifting boolean to move to ignore the checkCanMove. I need a major restructure / rethink
     checkIfInWall() {
       let atLeftWall
       let atRightWall
@@ -84,9 +91,9 @@ document.addEventListener('DOMContentLoaded', () => {
           atRightWall = index
         }
       }
-      if (!atLeftWall && !atRightWall) {
+      if (!atLeftWall || !atRightWall) {
         return false
-      } else if (atLeftWall < atRightWall) {
+      } else if (this.homeIndex % width === 0) {
         return 'inLeftWall'
       } else {
         return 'inRightWall'
@@ -100,9 +107,9 @@ document.addEventListener('DOMContentLoaded', () => {
         boardSquares[index].classList.add('has-active-block', this.color)
       })
       if (this.checkIfInWall() === 'inLeftWall') {
-        this.move('right')
+        this.move('right', true)
       } else if (this.checkIfInWall() === 'inRightWall') {
-        this.move('left')
+        this.move('left', true)
       }
     }
   }
@@ -210,47 +217,56 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  const z = new ZBlock(14)
-  const t = new TBlock(46)
-  const l = new LBlock(74)
-  const o = new OBlock(106)
-  const i = new IBlock(134)
-  const j = new JBlock(154)
-  const s = new SBlock(174)
+  const blockPrototypes = [TBlock, IBlock, JBlock, LBlock, SBlock, ZBlock, OBlock]
+  function generateBlock() {
+    const randomIndex = Math.floor(Math.random() * blockPrototypes.length)
+    return new blockPrototypes[randomIndex](5)
+  }
 
-  addEventListener('keydown', (e) => {
-    if (e.keyCode === 37) {
-      z.move('left')
-      t.move('left')
-      l.move('left')
-      i.move('left')
-      o.move('left')
-      j.move('left')
-      s.move('left')
-    } else if (e.keyCode === 39) {
-      z.move('right')
-      t.move('right')
-      l.move('right')
-      i.move('right')
-      o.move('right')
-      j.move('right')
-      s.move('right')
-    } else if (e.keyCode === 40) {
-      z.move('down')
-      t.move('down')
-      l.move('down')
-      i.move('down')
-      o.move('down')
-      j.move('down')
-      s.move('down')
-    } else if (e.keyCode === 38) {
-      z.rotate()
-      t.rotate()
-      l.rotate()
-      i.rotate()
-      o.rotate()
-      j.rotate()
-      s.rotate()
-    }
-  })
+  // !!! ----------- Testing Junk -----------
+
+  generateBlock().move()
+  // const z = new ZBlock(14)
+  // const t = new TBlock(46)
+  // const l = new LBlock(74)
+  // const o = new OBlock(106)
+  // const i = new IBlock(134)
+  // const j = new JBlock(154)
+  // const s = new SBlock(174)
+
+  // addEventListener('keydown', (e) => {
+  //   if (e.keyCode === 37) {
+  //     z.move('left')
+  //     t.move('left')
+  //     l.move('left')
+  //     i.move('left')
+  //     o.move('left')
+  //     j.move('left')
+  //     s.move('left')
+  //   } else if (e.keyCode === 39) {
+  //     z.move('right')
+  //     t.move('right')
+  //     l.move('right')
+  //     i.move('right')
+  //     o.move('right')
+  //     j.move('right')
+  //     s.move('right')
+  //   } else if (e.keyCode === 40) {
+  //     z.move('down')
+  //     t.move('down')
+  //     l.move('down')
+  //     i.move('down')
+  //     o.move('down')
+  //     j.move('down')
+  //     s.move('down')
+  //   } else if (e.keyCode === 38) {
+  //     z.rotate()
+  //     t.rotate()
+  //     l.rotate()
+  //     i.rotate()
+  //     o.rotate()
+  //     j.rotate()
+  //     s.rotate()
+  //   }
+  // })
 })
