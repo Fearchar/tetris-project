f4585231c14dce361212541351f2d10514787775
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-07-12 11:04:16 +0100

    Basic grid finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Working!')
+const width = 10
+const height = 20
+
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.querySelector('#board')
+  for (var i = 0; i < width * height; i++) {
+    board.innerHTML += `<div class="square">${i}</div>`
+  }
+})
